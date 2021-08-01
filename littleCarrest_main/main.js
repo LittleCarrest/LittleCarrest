7f5b1657ceaf6d72d14d0f2ff21fed500dea9c07
yudi <qleen513@gmail.com>
2021-08-02 04:38:13 +0900

    mainPage_first_commit

---
+++ littleCarrest_main/main.js
@@ -0,0 +1,30 @@
+$(function(){
+   
+    $('#fullpage').fullpage({
+        //options here
+        navigation:true,
+        autoScrolling:true,
+        // scrollBar:true,
+        
+        // afterLoad: function(anchorLink, index){
+        //     console.log("현재 섹션 번호는 " + index);
+        //     let nav = document.querySelector('#fp-nav ul span');
+        //     if(index==1){
+        //         nav.style.background='#f8f8f8';
+        //         nav.style.transition='0s';
+                
+        //     }else{
+        //         nav.style.background='#333';
+        //     }
+        // }
+
+    });
+
+})
+
+// if(scrollings.length > 149){
+//     let header = document.querySelector('header');
+//     header.style.opacity='0';
+// }
+
+
