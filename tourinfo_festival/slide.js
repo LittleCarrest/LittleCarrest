62704c8681b70521f02a2c4755262b3dcbba84b2
yudi <qleen513@gmail.com>
2021-08-15 14:52:57 +0900

    [update] 유진 수정본

---
+++ tourinfo_festival/slide.js
@@ -0,0 +1,16 @@
+
+$('.slide').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  fade: true
+});
+
+$('.bottom_slide').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+});
